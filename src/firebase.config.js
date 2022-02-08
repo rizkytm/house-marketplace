e5f13b6865627f7814302db69bb5f83657b465be
rizkytm <ritrime97@gmail.com>
2022-02-08 21:19:34 +0700

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyCaglfjCMY39cOWOPfNHKrm5wPep78DPXg',
+  authDomain: 'house-marketplace-app-dd31a.firebaseapp.com',
+  projectId: 'house-marketplace-app-dd31a',
+  storageBucket: 'house-marketplace-app-dd31a.appspot.com',
+  messagingSenderId: '829962163624',
+  appId: '1:829962163624:web:cc76dd41771cf86256b99d'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
